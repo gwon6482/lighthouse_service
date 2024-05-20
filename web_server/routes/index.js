c86f035e89c1c143a1c20118bbc99484b8ee0e04
gwon6482 <pdsjw991020@gmail.com>
2024-05-20 16:23:20 +0900

    dist 통합 테스트

--- web_server/routes/index.js
+++ web_server/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
-/* GET home page. */
+
+/* GET index page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LightHouse' });
+    res.sendFile(path.join(__dirname, '../views/temp_home.html'));
 });
 
+
+router.get('/home', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../views/home.html'));
+});
+  
 module.exports = router;
