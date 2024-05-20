869332536affc66820d950e53ff5c1dbc8298a03
winkslee <jakejhlee0210@gmail.com>
2024-05-20 15:23:09 +0900

    vue template test

---
+++ vue_dev/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1> {{ message }}</h1>
+  <HelloWorld />
+  <h2>
+    Test
+  </h2>
+</template>
+
+<script>
+import HelloWorld from '~/components/HelloWorld'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: 'Hello Vue!!!'
+    }
+  }
+}
+</script>
+
+<style>
+  h2 {
+    color: orange;
+    font-size: 40px;
+    display: flex;
+  }
+</style>
